8c7179f6ff9152f78221bfc0d9cba54cea7d886b
Alejandro Vera <avera@conwet.com>
2013-12-04 08:40:58 +0000

    Geonames cargado por defecto

--- js/conwet/Gadget.js
+++ js/conwet/Gadget.js
@@ -58,14 +58,29 @@ conwet.Gadget = Class.create({
         this.serviceConfiguration = null; //Contains the configuration of the service in use
         this.serviceConfigurationList = []; //Contains the configuration of all the services
         
-        this.configSlot   = new conwet.events.Slot('config_slot', function(service) {
-            service = JSON.parse(service);
-            if ((typeof service == 'object') && ('type' in service) && ('url' in service) &&
-                    ('service_type' in service) && ('name' in service) && (service.type == "WFS") &&
-                    (service.service_type == "GEONAMES") && (service.url != "")) {
-                this.loadNewService(service, true);
-            }
-        }.bind(this));
+        new Ajax.Request("js/service-config/geonames.xml", {
+            method: 'GET',
+            onSuccess: function(transport) {
+
+                this.serviceConfiguration = XMLObjectifier.xmlToJSON(transport.responseXML);
+                
+                this.service = {
+                    name : 'Geonames', 
+                    url: 'http://api.geonames.org/search',
+                    service_type: 'GEONAMES',
+                    type: 'WFS',
+                    xmlText: transport.responseText
+                };
+                
+                this.controller = new conwet.GeoNamesController(this);
+                
+                this.draw();
+
+            }.bind(this),
+            onFailure: function(transport) {
+                this.showMessage(_("Error al cargar la configuración del servicio"));
+            }.bind(this)
+        });
 
         this.servicesPreference = MashupPlatform.widget.getVariable("services");
         
@@ -74,38 +89,11 @@ conwet.Gadget = Class.create({
         this.transformer    = new conwet.map.ProjectionTransformer();
         
         this.parseUtils = new conwet.parser.ParseUtils();
-
-        this.draw();
     },
 
     draw: function() {
         var header = $("header");
         conwet.ui.UIUtils.ignoreEvents(header, ["click", "dblclick"]);
-
-        var serviceLabel = document.createElement("div");
-        $(serviceLabel).addClassName("label");
-        serviceLabel.appendChild(document.createTextNode(_("Servicio:")));
-        header.appendChild(serviceLabel);
-
-        //Service selector
-        this.serviceSelect = new StyledElements.StyledSelect();
-        this.serviceSelect.addEventListener("change", function(){
-            if(this.serviceSelect.getValue() != "")
-                this.setWfsService(JSON.parse(this.serviceSelect.getValue()));
-        }.bind(this));
-        this.serviceSelect.addClassName("service");
-        this.serviceSelect.textDiv.hide();
-        this.serviceSelect.insertInto(header);
-
-        this.serviceSelect.addEntries([{label: _('Select a server'), value: ''}]);
-
-        if (this.servicesPreference.get() != "") {
-            var services = JSON.parse(this.servicesPreference.get());
-
-            for (var i=0; i<services.length; i++) {
-                this.loadNewService(services[i], i==0);
-            }
-        }
 
         var searchLabel = document.createElement("div");
         $(searchLabel).addClassName("label");
@@ -148,89 +136,7 @@ conwet.Gadget = Class.create({
     
     launchSearch: function(){
         this.sendSearch(this.searchInput.value);
-        this.controller._sendSearchRequest(JSON.parse(this.serviceSelect.getValue()), this.searchInput.value, this.propertySelect.getValue());  
-    },
-            
-    /*
-     * This functions adds a WFS service to the select. If added, returns true, othrewise returns false.
-     */
-    loadNewService: function(service, selected) {
-        var serviceJson = JSON.stringify(service);
-        
-        //Add it if it already isn't in the select
-        if(!(serviceJson in this.serviceSelect.optionValues)){
-            if(service.xmlText != null){
-                var configuration = XMLObjectifier.xmlToJSON(XMLObjectifier.textToXML(service.xmlText));
-                this.addNewService(service, configuration, selected);
-            }
-        }
-        
-    },
-
-    addNewService: function(service, configuration, selected){
-        var serviceJson = JSON.stringify(service);
-        
-        this.serviceSelect.addEntries([{label: service.name, value: serviceJson}]);
-                    
-        //Add the configuration to the list of configurations
-        this.serviceConfigurationList[service.name] = configuration;
-
-        //Set this as the current service
-        if(selected)
-            this.setWfsService(service);
-
-        //Tell everything is ok and save the services list (persistent list)
-        this.showMessage(_("Se ha recibido un nuevo servidor."));
-        this.save(service);  
-    },
-    
-    /*
-     * This function changes the current service to the given service
-     */
-    setWfsService: function(service){
-        this.serviceSelect.setValue(JSON.stringify(service));
-        
-        //Parse the XML configuration to an object
-        this.serviceConfiguration = this.serviceConfigurationList[service.name];
-
-        /*
-        //Set the search options list
-        this.propertySelect.clear();
-        try{
-            var searchOptions = this.serviceConfiguration.request[0].search[0].option;
-            for(var x = 0; x < searchOptions.length; x++){
-                var propertyName = searchOptions[x].Text;
-                var label = searchOptions[x].label;
-                this.propertySelect.addEntries([{label: _(label), value: propertyName}]);
-            }
-        }catch(e){};
-        */
-        
-        this.controller = new conwet.GeoNamesController(this);
-        
-        //Clean the list of results
-        this.clearUI();
-        
-    },
-
-    /*
-     * This function saves the service in a persistent list.
-     */
-    save: function(service) {
-        var services = [];
-        if(this.servicesPreference.get() != "")
-            services = JSON.parse(this.servicesPreference.get());
-        
-        var found = false;
-        for(var i = 0; i < services.length; i++){
-            if(services[i].url == service.url)
-                found = true;
-        }
-        
-        if(!found){
-            services.push(service);
-            this.servicesPreference.set(JSON.stringify(services));
-        }
+        this.controller._sendSearchRequest(this.service, this.searchInput.value, this.propertySelect.getValue());  
     },
 
     /*
